81c1898b4a2b36f2cdca63707d4f8524ede7306a
JOSHIK <jn2044@hw.ac.uk>
2024-05-01 03:05:31 +0100

    width adjustments

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -4,7 +4,7 @@ import SheetSide from "./ui/postSheet";
 export default async function Post() {
   const user = await currentUser();
   return (
-    <div className="bg-white w-[550px] rounded-lg mx-auto mt-4">
+    <div className="bg-white w-[400px] lg:w-[550px] rounded-lg mx-auto mt-4">
       <div className="flex justify-evenly pb-4">
         <img src={user?.imageUrl} className="rounded-full w-[48px] mt-4"></img>
         <SheetSide />
