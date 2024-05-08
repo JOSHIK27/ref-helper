425ace350db92c54c882cce06ad2b872f10ec836
JOSHIK <jn2044@hw.ac.uk>
2024-05-08 23:13:20 +0100

    minor testing

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -17,16 +17,16 @@ export default function Notifications() {
           table: "messages",
         },
         (payload) => {
+          toast({
+            title: `Message From ${payload.new.from}`,
+            description: payload.new.message,
+          });
+          // if (user.isLoaded && user.user?.firstName == payload.new.to) {
           //   toast({
           //     title: `Message From ${payload.new.from}`,
           //     description: payload.new.message,
           //   });
-          if (user.isLoaded && user.user?.firstName == payload.new.to) {
-            toast({
-              title: `Message From ${payload.new.from}`,
-              description: payload.new.message,
-            });
-          }
+          // }
         }
       )
       .subscribe();
